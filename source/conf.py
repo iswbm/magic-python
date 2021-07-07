6d0b4ff94655fa96d65cf36246cb38f9b9b31c0c
BingmingWong <wongbingming@163.com>
2021-07-07 21:27:31 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -28,7 +28,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['chinese_search','sphinx.ext.mathjax']
+extensions = ['chinese_search','sphinx.ext.mathjax','sphinx_sitemap']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
