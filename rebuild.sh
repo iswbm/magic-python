fd160ce49a83642857285f2ab05cd539ed3973df
iswbm <wongbingming@163.com>
2022-02-20 13:09:49 +0800

    add rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,13 @@
+cat << EOF >/usr/local/lib/python3.6/site-packages/sphinx_rtd_theme/comments.html
+<comments>
+  <script src="https://utteranc.es/client.js"
+    repo="iswbm/magic-python"
+    issue-term="pathname"
+    theme="github-light"
+    crossorigin="anonymous"
+    async>
+  </script>
+</comments>
+EOF
+
+rm -rf build/ && sphinx-multiversion source build/html && cp -rf build/html/master/* build/html/
